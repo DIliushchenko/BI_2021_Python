74b0f9db7e3448c3da554b9a419214e8cc90de18
diliushchenko <serioussammdk@gmail.com>
2021-10-04 21:27:30 +0200

    another update for flake

--- seq_fun_hw1.py
+++ seq_fun_hw1.py
@@ -1,20 +1,22 @@
 print('Hello, welcome to my project!')
 
-#fuction to define dna it's or not
+# fuction to define dna it's or not
 def are_u_natural(sequence):
-    
+
+
     sequence = set(sequence)
-        
-    if ('T' in sequence) and ('U' in sequence):  #delete case when T and U in seq
+
+    if ('T' in sequence) and ('U' in sequence):  # delete case when T and U in seq
         return False
-        
+
     return sequence.issubset("ATGCU")
 
-        
+
 def transcribe(sequence):
-    
+
+
     trans_seq = []
-    
+
     for base in sequence:
         if base == 'T':
             trans_seq.append('U')
@@ -22,19 +24,20 @@ def transcribe(sequence):
             trans_seq.append('u')
         else:
             trans_seq.append(base)
-        
+
     return "".join(trans_seq)
 
 
 def complement(sequence):
-    
+
+
     if 'T' in sequence:
-        type_nuc = 'dna'   
+        type_nuc = 'dna'
     elif 'U' in sequence:
         type_nuc = 'rna'
-     
+
     compl_seq = []
-    
+
     for base in sequence:
         # it's important! in dna A goes to T, but in rna A goes to T
         if type_nuc == 'dna' and base == 'A':
@@ -61,23 +64,23 @@ def complement(sequence):
             compl_seq.append('C')
         elif base == 'g':
             compl_seq.append('c')
-    
+
     return "".join(compl_seq)
 
 
-list_of_functions = {'transcribe','reverse','complement','reverse complement','exit'}  #try use soft instead list
+list_of_functions = {'transcribe', 'reverse', 'complement', 'reverse complement', 'exit'}  # try use soft instead list
 
 # Let's type our commands!!!!
 while True:
-    
+
     print('Fuctions you can use: \n', str(list_of_functions))
     fun_dna = input('Enter command:')
-    
-    # check that user typed exicted 
+
+    # check that user typed exicted
     if fun_dna not in list_of_functions:
         print('Invalid function. Try Again!')
         continue
-    
+
     elif fun_dna == 'exit':
         print('Good luck')
         break
@@ -87,13 +90,13 @@ while True:
         upper_seq = seq.upper()
 
         # Check if it's dna/rna.
-        
+
         while ~are_u_natural(upper_seq):
-            
+
             print('Invalid alphabet. Try again!')
             seq = (input('Enter sequence:'))
             upper_seq = seq.upper()
-        
+
         # use function which user typed
         if fun_dna == 'transcribe':
 
@@ -110,4 +113,3 @@ while True:
             print(complement(seq))
         elif fun_dna == 'reverse complement':
             print(complement(seq[::-1]))
-            

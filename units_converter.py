0012f9a284eb6e7e956cf5bd5dae89a97af83606
DIliushchenko <iliushchenkodmitrii@gmail.com>
2021-10-05 00:20:50 +0200

    better for flake

--- units_converter.py
+++ units_converter.py
@@ -5,25 +5,19 @@ num_temp = int(temperature[:-1])  # take all numbers except last symbol
 convert_from = temperature[-1]
 
 if convert_from.upper() == "C" and convert_to.upper() == 'F':
-  res = int(round((num_temp * (9/5) + 32)))
-  
+    res = int(round((num_temp * (9/5) + 32)))  
 elif convert_from.upper() == "C" and convert_to.upper() == 'R':
-  res = int(round((num_temp + 273.15) * (9/5)))
-
+    res = int(round((num_temp + 273.15) * (9/5)))
 elif convert_from.upper() == "F" and convert_to.upper() == 'C':
-  res = int(round((num_temp - 32) * 5 / 9))
-
+    res = int(round((num_temp - 32) * 5 / 9))
 elif convert_from.upper() == "F" and convert_to.upper() == 'R':
-  res = int(round(num_temp + 459.67))
-
+    res = int(round(num_temp + 459.67))
 elif convert_from.upper() == "R" and convert_to.upper() == 'C':
-  res = int(round((num_temp - 491.67) * 5 / 9))
-
+    res = int(round((num_temp - 491.67) * 5 / 9))
 elif convert_from.upper() == "R" and convert_to.upper() == 'F':
-  res = int(round(num_temp - 459.67))
-
+    res = int(round(num_temp - 459.67))
 else:
-  print("Input proper convention.")
-  quit()
+    print("Input proper convention.")
+    quit()
 
 print("You converted", temperature, "to", str(res)+convert_to)

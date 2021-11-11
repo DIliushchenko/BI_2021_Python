aa264953c26db3db1535acf7b83dde690a86553a
DIliushchenko <iliushchenkodmitrii@gmail.com>
2021-11-11 11:28:25 +0200

    final version of hw

--- numpy_challenge.py
+++ numpy_challenge.py
@@ -20,18 +20,12 @@ def matrix_multiplication(A, B):
 
 
 def multiplication_check(mat_list):
+    res = np.zeros((np.shape(mat_list[0])[0],np.shape(mat_list[0])[1]))
     for i in range(1, len(mat_list)):
-        if i == 1:
-            if np.shape(mat_list[0])[1] != np.shape(mat_list[1])[0]:
-                return False
-            else:
-                res = np.zeros((np.shape(mat_list[0])[0], np.shape(mat_list[1])[1]), dtype=int)
+        if np.shape(res)[1] != np.shape(mat_list[i])[0]:
+            return False
         else:
-            if np.shape(res)[1] != np.shape(mat_list[i])[0]:
-                return False
-            else:
-                res = np.zeros((np.shape(res)[0], np.shape(mat_list[i])[1]), dtype=int)
-
+            res = np.zeros((np.shape(res)[0], np.shape(mat_list[i])[1]))
     return True
 
 
@@ -45,3 +39,34 @@ def multiply_matrices(mat_list):
         return res
     else:
         return None
+
+
+def compute_2d_distance(a1, a2):
+
+    x = (a2[0]-a1[0]) ** 2
+    y = (a2[1]-a1[1]) ** 2
+    return (x + y) ** 0.5
+
+
+def compute_multidimensional_distance(a1, a2):
+
+    if len(a1) != len(a2):
+        return None
+    else:
+        res = 0
+        for i in range(len(a1)):
+            res += (a2[i]-a1[i]) ** 2
+        return res ** 0.5
+
+
+def compute_pair_distances(matrix):
+
+    res = np.zeros((np.shape(matrix)[0],np.shape(matrix)[0]))
+
+    for i in range(np.shape(matrix)[0]):
+
+        for j in range(np.shape(matrix)[0]):
+
+            res[i][j] = compute_multidimensional_distance(matrix[i], matrix[j])
+
+    return res
